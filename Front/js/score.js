3e7bb27aad5f363f63c6fe3af84811eb5f360f13
Lionel <lionel.smets@isen.yncrea.fr>
2021-05-27 10:45:49 +0200

    fixed last errors

--- Front/js/score.js
+++
@@ -1,9 +0,0 @@
-let userId = document.getElementById("usernameId");
-
-socket.emit('nameSession', '');
-
-socket.on('onSession', data => {
-    if(data) {
-        userId.innerHTML = data;
-    }
-})
